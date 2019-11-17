edcdf3872c72d81173964988a95b3ed632b0cfba
Don Petersen <don@donpetersen.net>
2019-11-16 18:40:53 -0800

    Fix warnings, remove backtrace comment
    The Write trait didn't get used in the final version of the code. And
    the load_keys call I removed was part of the initial implementation and
    was getting called twice, and it was warning about the map in the first
    call not actually being used.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 use serde_json::Value;
 use std::collections::HashMap;
 use std::io::Read;
-use std::io::Write;
 use std::str::FromStr;
 use structopt::StructOpt;
 
@@ -39,8 +38,6 @@ impl FromStr for Subcommand {
 fn main() {
     let opt = Opt::from_args();
     println!("{:#?}", opt);
-
-    let map = load_keys().unwrap();
 
     match opt.subcommand {
         Subcommand::Get => unimplemented!(),
@@ -89,66 +86,3 @@ fn load_keys() -> std::io::Result<HashMap<String, String>> {
         _ => panic!("Not a Map: CORRUPT DATABASE!!!"),
     }
 }
-
-/*
-❯ RUST_BACKTRACE=1 ./target/debug/kv set --key brian --value hello
-Opt {
-    subcommand: Set,
-    key: "brian",
-    value: "hello",
-}
-thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Os { code: 9, kind: Other, message: "Bad file descriptor" }', src/libcore/result.rs:1165:5
-stack backtrace:
-   0: backtrace::backtrace::libunwind::trace
-             at /Users/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/backtrace-0.3.37/src/backtrace/libunwind.rs:88
-   1: backtrace::backtrace::trace_unsynchronized
-             at /Users/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/backtrace-0.3.37/src/backtrace/mod.rs:66
-   2: std::sys_common::backtrace::_print_fmt
-             at src/libstd/sys_common/backtrace.rs:76
-   3: <std::sys_common::backtrace::_print::DisplayBacktrace as core::fmt::Display>::fmt
-             at src/libstd/sys_common/backtrace.rs:60
-   4: core::fmt::write
-             at src/libcore/fmt/mod.rs:1030
-   5: std::io::Write::write_fmt
-             at src/libstd/io/mod.rs:1412
-   6: std::sys_common::backtrace::_print
-             at src/libstd/sys_common/backtrace.rs:64
-   7: std::sys_common::backtrace::print
-             at src/libstd/sys_common/backtrace.rs:49
-   8: std::panicking::default_hook::{{closure}}
-             at src/libstd/panicking.rs:196
-   9: std::panicking::default_hook
-             at src/libstd/panicking.rs:210
-  10: std::panicking::rust_panic_with_hook
-             at src/libstd/panicking.rs:473
-  11: std::panicking::continue_panic_fmt
-             at src/libstd/panicking.rs:380
-  12: rust_begin_unwind
-             at src/libstd/panicking.rs:307
-  13: core::panicking::panic_fmt
-             at src/libcore/panicking.rs:85
-  14: core::result::unwrap_failed
-             at src/libcore/result.rs:1165
-  15: core::result::Result<T,E>::unwrap
-             at /rustc/4560ea788cb760f0a34127156c78e2552949f734/src/libcore/result.rs:933
-  16: kv::main
-             at src/main.rs:47
-  17: std::rt::lang_start::{{closure}}
-             at /rustc/4560ea788cb760f0a34127156c78e2552949f734/src/libstd/rt.rs:64
-  18: std::rt::lang_start_internal::{{closure}}
-             at src/libstd/rt.rs:49
-  19: std::panicking::try::do_call
-             at src/libstd/panicking.rs:292
-  20: __rust_maybe_catch_panic
-             at src/libpanic_unwind/lib.rs:80
-  21: std::panicking::try
-             at src/libstd/panicking.rs:271
-  22: std::panic::catch_unwind
-             at src/libstd/panic.rs:394
-  23: std::rt::lang_start_internal
-             at src/libstd/rt.rs:48
-  24: std::rt::lang_start
-             at /rustc/4560ea788cb760f0a34127156c78e2552949f734/src/libstd/rt.rs:64
-  25: <kv::Subcommand as core::fmt::Debug>::fmt
-note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
-*/
